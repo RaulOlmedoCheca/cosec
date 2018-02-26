e9eacad183c330074ac07fb19831677df753fe2e
Raul Olmedo <raul.olmedo.dev@gmail.com>
2018-02-26 15:14:22 +0100

    JQPEFOWGW

--- deliverable_1_2.sh
+++ deliverable_1_2.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
-sudo apt-get install openssh-server && openssh-client -y
+sudo apt-get install openssh-server openssh-client -y 
 
-sudo iptables -A INPUT -p all --dport 22 -d 127.0.0.1 -j LOG
-sudo iptables -A INPUT -p all --dport 22 -d 127.0.0.1 -j DROP
-sudo iptables -A INPUT -p all --dport 22 -s 192.168.x.x -d 127.0.0.1 -j ACCEPT
+sudo iptables -A INPUT -p all -d 127.0.0.1 --dport 22  -j LOG #Destino localhost esta bien o es redundante?
+sudo iptables -A INPUT -p all -d 127.0.0.1 --dport 22  -j DROP
+sudo iptables -A INPUT -p all -s 192.168.x.x -d 127.0.0.1 --dport 22 -j ACCEPT
