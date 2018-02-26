6bd87fb971c522a20bfdc7a972dbc30e0a147d56
Raul Olmedo <raul.olmedo.dev@gmail.com>
2018-02-26 15:31:43 +0100

    Testing

--- deliverable_1_2.sh
+++ deliverable_1_2.sh
@@ -1,9 +1,9 @@
 sudo apt-get update
 sudo apt-get install openssh-server openssh-client -y 
 
-sudo iptables -A INPUT -p tcp -d 127.0.0.1 --dport 22  -j LOG #Destino localhost esta bien o es redundante?
-sudo iptables -A INPUT -p tcp -d 127.0.0.1 --dport 22  -j DROP
-sudo iptables -A INPUT -p tcp -s 192.168.x.x -d 127.0.0.1 --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22  -j LOG #Destino localhost esta bien o es redundante?
+sudo iptables -A INPUT -p tcp --dport 22  -j DROP
+sudo iptables -A INPUT -p tcp -d 127.0.0.1 --dport 22 -j ACCEPT
 # como los pings se loguean tambien:
 sudo iptables -A INPUT -p icmp -d 127.0.0.1 -j LOG
 sudo iptables -A INPUT -p icmp -d 127.0.0.1 -j DROP
