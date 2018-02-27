d00c26ecd98f66c157e3f08e11c9c3ae9093adfa
Raul Olmedo <raul.olmedo.dev@gmail.com>
2018-02-27 17:14:03 +0100

    sefsegs

--- deliverable_1_2.sh
+++ deliverable_1_2.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-MYIP = 163.117.244.97
-IPFROMADMIN = 163.117.221.151
+MYIP=163.117.244.97
+IPFROMADMIN=163.117.221.151
 
+# Update headers to install openssh
 sudo apt-get update
+# Install openssh in order to allow ssh connections into the server
 sudo apt-get install openssh-server openssh-client -y 
 
+# Change firewall default policy to drop all incoming packets
 sudo iptables -P INPUT DROP
-sudo iptables -A INPUT -p tcp --dport ssh -s $IPFROMADMIN -d $MYIP -j ACCEPT # no funciona en loopback
+sudo iptables -A INPUT -p tcp -d $MYIP --dport ssh -s $IPFROMADMIN  -j ACCEPT # no funciona en loopback
  
 # acepta todas las conexiones entrantes que ya hayan sido establecidas o relacionadas con una peticon DNS
-sudo iptables -A INPUT -p udp --sport domain -m state --state RELATED,ESTABLISHED -d $MYIP -j ACCEPT 
+sudo iptables -A INPUT -p udp -d $MYIP --sport domain -m state --state RELATED,ESTABLISHED -j ACCEPT 
+sudo iptables -A INPUT -p tcp -d $MYIP --sport domain -m state --state RELATED,ESTABLISHED -j ACCEPT 
 # acepta todas las conexiones entrantes que ya hayan sido establecidas o relacionadas con una peticion saliente del puerto HTTP del origen (por ejemplo cuando se ejecuta apt-get update)
-sudo iptables -A INPUT -p tcp --sport http -m state --state RELATED,ESTABLISHED -d $MYIP -j ACCEPT 
+sudo iptables -A INPUT -p tcp -d $MYIP --sport http -m state --state RELATED,ESTABLISHED -j ACCEPT 
 
-nano /var/log/kern.log
