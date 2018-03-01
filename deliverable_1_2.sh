71767f7f95e3700c00b1986c3514ddbab3a06a62
Raul Olmedo <raul.olmedo.dev@gmail.com>
2018-03-01 20:22:46 +0100

    Diria que final commit

--- deliverable_1_2.sh
+++ deliverable_1_2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MYIP=192.168.56.101
-IPFROMADMIN=163.168.56.1
+IPFROMADMIN=192.168.56.1
 
 # Update headers to install openssh
 sudo apt-get update
@@ -12,25 +12,28 @@ sudo apt-get install openssh-server openssh-client -y
 sudo iptables -P INPUT DROP
 sudo iptables -P OUTPUT DROP
 
-#Enable input packets in loopback interface (apache - mysql use it)
+# Enable input packets in loopback interface (apache - mysql use it)
 sudo iptables -A INPUT -i lo -j ACCEPT
 sudo iptables -A OUTPUT -o lo -j ACCEPT
 
-#Accept only ssh connections from the admin's ip
+# Accept only ssh connections from the admin's ip
 sudo iptables -A INPUT -p tcp -d $MYIP --dport ssh -s $IPFROMADMIN  -m state --state NEW -j ACCEPT # lo mismo ip destino sobra
 # sudo iptables -A OUTPUT -p tcp -d $IPFROMADMIN -s $MYIP --sport ssh -m state --state RELATED,ESTABLISHED  -j ACCEPT 
 
 # Accept al incoming connections to apache-tomcat
 sudo iptables -A INPUT -p tcp -d $MYIP --dport http -m state --state NEW -j ACCEPT 
-
-#sudo iptables -A OUTPUT -p tcp -d $MYIP --dport http -j ACCEPT
+# Checks if trhe connection is new in the last minute, and blocks it if 10 petitions has been sent.
+sudo iptables -A INPUT -p tcp -d $MYIP --dport http -m state --state NEW -m recent --update --seconds 60 --hitcount 10 -j DROP
  
 
-#TODO
+# Allow outgoing connections to dns and http ports in order to make apt-get update possible
 sudo iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport http -m state --state NEW -j ACCEPT
 sudo iptables -A OUTPUT -p tcp --dport domain -m state --state NEW -j ACCEPT
 sudo iptables -A OUTPUT -p udp --dport domain -m state --state NEW -j ACCEPT
 
+# Allow all related input connections (because we control output traffic it is secure)
 sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT 
 
+# Start snort alert system
+sudo snort -c /etc/snort/snort.conf -l /var/log/snort -A console -D
